8ef99feeaed6146152c621be1d8794fd7a9816b6
ML-std <ML-std@github. com>
2024-02-12 19:54:53 +0300

    add universe repo to ubuntu

--- cd/web_server/script.sh
+++ cd/web_server/script.sh
@@ -1,8 +1,9 @@
   #!/bin/bash -xe
   sudo apt update
+  sudo apt-add-repository universe -y
   sudo apt install -y git
   sudo apt install -y awscli
-  API_KEY=$(aws ssm get-parameter --name "/gpt_api_key" --with-decryption --query "Parameter.Value" --output text)
+  API_KEY=$(aws --region eu-west-2 ssm get-parameter --name "/gpt_api_key" --with-decryption --query "Parameter.Value" --output text)
   sudo apt install -y python3
   sudo git clone https://github.com/ML-std/finance-cbot.git
   sudo cd ./finance-cbot
