c5aeb7828e922406a14d46b5e6efca973ed24a72
Ali Onur Aslan <onuras0399@hotmail.com>
2024-02-13 04:05:07 +0300

    plot CPI graph more countries for CPI more beaitiful data getting process

--- stockGPT.py
+++ stockGPT.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import config
 import logging
 from streamlit.runtime.scriptrunner import get_script_run_ctx
@@ -10,18 +10,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 st.title("BIGDaTA_Lab Chatbot")
-openai.api_key = "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"
-logging.basicConfig(filename='C:\\Users\\Ali Onur\\Desktop\\finbot\\logs\\stockGPT.logstockGPT.log',
+openai.api_key = "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"
+logging.basicConfig(filename='../logs/stockGPT.logstockGPT.log',
                     level=logging.INFO, format='%(asctime)s - %(message)s', encoding='utf-8')  # encoding ekleyin
 
-CPI_Data_Address = "C:\\Users\\Ali Onur\\Desktop\\finbot\\finance-cbot\\CPI_DATA.xls"
+CPI_Data_Address = "./Country_Indexes_And_Weights.xlsx"
 
 # Veriyi yükle
-with open('C:\\Users\\Ali Onur\\Desktop\\finbot\\finance-cbot\\training_data.json', 'r', encoding='utf-8') as f:
+with open('./training_data.json', 'r', encoding='utf-8') as f:
     veri = json.load(f)
-with open('C:\\Users\\Ali Onur\\Desktop\\finbot\\finance-cbot\\hesap_botu.json', 'r', encoding='utf-8') as f:
+with open('./hesap_botu.json', 'r', encoding='utf-8') as f:
     veri2 = json.load(f)
-
 
 def get_stock_price(ticker):
     try:
@@ -30,24 +29,150 @@ def get_stock_price(ticker):
         return f"Veri alınırken bir hata oluştu: {e}"
 
 
-def get_CPI(country_code, year, month):
+def get_CPI(country_code, subject_name="Consumer Price Index, All items", year="2023", month="12"):
     # Get Consumer Price Index
+
+    subjectNameToNumber = {
+        "Consumer Price Index, All items": 0,
+        "Food and non-alcoholic beverages": 1,
+        "Alcoholic Beverages, Tobacco, and Narcotics": 2,
+        "Clothing and footwear": 3,
+        "Housing, Water, Electricity, Gas and Other Fuels": 4,
+        "Furnishings, routine household maintenance": 5,
+        "Health": 6,
+        "Transport": 7,
+        "Communication": 8,
+        "Recreation and culture": 9,
+        "Education": 10,
+        "Restaurants and hotels": 11,
+        "Miscellaneous goods and services": 12
+    }
+
+    if int(year) > 2024:
+        return "Future values cannot be predicted"
+    elif (int(year) == 2024):
+        return "Statistics of 2024 is not yet shared"
+
     data_file = pd.read_excel(CPI_Data_Address, sheet_name=country_code, header=None)
-    cell_value = data_file.iloc[int(year) - 1999, int(month)]
+
+    if country_code == "NL":
+        columnIndex = ((int(year) - 1960) * 12) + int(month) - 3
+
+        if columnIndex > 0:
+            cell_value = data_file.iloc[5 + subjectNameToNumber[subject_name], columnIndex]
+        else:
+            return "Required statistics for the Netherlands is not recorded at that time"
+
+        return f"Considering 2014 = 100, {month}.{year} = " + str(cell_value)
+    elif country_code == "TR":
+        columnIndex = ((int(year) - 1955) * 12) + int(month)
+
+        if columnIndex > 0:
+            cell_value = data_file.iloc[5 + subjectNameToNumber[subject_name], columnIndex]
+        else:
+            return "Required statistics for Turkey is not recorded at that time"
+
+        return f"Considering 2003 = 100, {month}.{year} = " + str(cell_value)
+    elif country_code == "IT":
+        columnIndex = ((int(year) - 1955) * 12) + int(month)
+
+        if columnIndex > 0:
+            cell_value = data_file.iloc[5 + subjectNameToNumber[subject_name], columnIndex]
+        else:
+            return "Required statistics for Italy is not recorded at that time"
+
+        return f"Considering 2014 = 100, {month}.{year} = " + str(cell_value)
+    elif country_code == "GR":
+        columnIndex = ((int(year) - 1955) * 12) + int(month)
+
+        if columnIndex > 0:
+            cell_value = data_file.iloc[5 + subjectNameToNumber[subject_name], columnIndex]
+        else:
+            return "Required statistics for Greece is not recorded at that time"
+
+    return f"Considering 2014 = 100, {month}.{year} = " + str(cell_value)
+
     return str(cell_value)
 
 
-def plot_CPI(country_code, start_year, start_year_month, end_year, end_year_month):
-    # TODO
-    # data = yf.Ticker(ticker).history(period="1y")
-    # plt.figure(figsize=(10, 5))
-    # plt.plot(data.index, data.Close)
-    # plt.title(f'{ticker} Geçen Yıla Göre Hisse Fiyatı')
-    # plt.xlabel('Tarih')
-    # plt.ylabel('Hisse Fiyatı ')
-    # plt.grid(True)
-    # plt.savefig('stock.png')
-    # plt.close()
+def plot_CPI(country_code, start_year="2000", start_month="1", end_year="2023", end_month="12"):
+    date_arr = []
+    cpi_arr = []
+    base_year = ""
+
+    data_file = pd.read_excel(CPI_Data_Address, sheet_name=country_code, header=None)
+
+    start_date = datetime(int(start_year), int(start_month), 28)
+    end_date = datetime(int(end_year), int(end_month), 1)
+
+    current_date = start_date
+    while current_date <= end_date:
+        str_date = current_date.strftime("%m-%Y")
+        date_arr.append(str_date)
+        current_date += timedelta(days=30)  # Increment by one month
+
+        year = current_date.strftime("%Y")
+        month = current_date.strftime("%m")
+
+        if country_code == "NL":
+            columnIndex = ((int(year) - 1960) * 12) + int(month) - 4
+            if columnIndex > 0:
+                cpi_arr.append(data_file.iloc[5, columnIndex])
+            else:
+                cpi_arr.append(0)
+            base_year = "2014 = 100"
+        elif country_code == "TR":
+            columnIndex = ((int(year) - 1955) * 12) + int(month) - 1
+            if columnIndex > 0:
+                cpi_arr.append(data_file.iloc[5, columnIndex])
+            else:
+                cpi_arr.append(0)
+            base_year = "2003 = 100"
+        elif country_code == "IT":
+            columnIndex = ((int(year) - 1955) * 12) + int(month) - 1
+            if columnIndex > 0:
+                cpi_arr.append(data_file.iloc[5, columnIndex])
+            else:
+                cpi_arr.append(0)
+            base_year = "2014 = 100"
+        elif country_code == "GR":
+            columnIndex = ((int(year) - 1955) * 12) + int(month) - 1
+
+            if columnIndex > 0:
+                cpi_arr.append(data_file.iloc[5, columnIndex])
+            else:
+                cpi_arr.append(0)
+            base_year = "2014 = 100"
+    # Creating a DataFrame from a dictionary
+    data = {
+        'date': date_arr,
+        'cpi': cpi_arr,
+    }
+
+    df = pd.DataFrame(data)
+    df.set_index('date', inplace=True)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(df.index, df.cpi)
+
+    # Rotate the x-axis labels
+    plt.xticks(rotation=45)
+
+    # Get the current ticks
+    ticks = plt.xticks()[0]
+
+    # Adjust the number of ticks to match the number of labels
+    desired_ticks = 10  # Choose the desired number of ticks
+    step = len(df.index) // desired_ticks
+    plt.xticks(ticks[::step])
+
+
+    plt.title(f'{country_code} CPI Graph {start_month}.{start_year} - {end_month}.{end_year} ({base_year})')
+    plt.xlabel('Date')
+    plt.ylabel('CPI Value ')
+    plt.grid(True)
+    plt.savefig('CPI.png')
+    plt.close()
     return 0
 
 
@@ -110,13 +235,14 @@ available_fuctions = {
     'calculate_MACD': calculate_MACD,
     'plot_stock_price': plot_stock_price,
     'get_CPI': get_CPI,
+    'plot_CPI': plot_CPI,
     'report': report
 }
 
 functions = [
     {
         'name': 'get_CPI',
-        'description': 'Gets the consumer price index of country in the given time',
+        'description': 'Gets the consumer price index of country in the given time.',
         'parameters': {
             'type': 'object',
             'properties': {
@@ -133,6 +259,47 @@ functions = [
                 'month': {
                     'type': 'string',
                     'description': "This value represents the numerical value of desired month of the CPI value for the desired country. (Ex. 7 for july)"
+
+                },
+                # 'subject_name': {
+                #    'type': 'string',
+                #    'description': "This value represents the data of the subject required. The options are: Consumer Price Index, All items; Food and non-alcoholic beverages; Alcoholic Beverages, Tobacco, and Narcotics; Clothing and footwear; Housing, Water, Electricity, Gas and Other Fuels; Furnishings, routine household maintenance; Health; Transport; Communication; Recreation and culture; Education; Restaurants and hotels; Miscellaneous goods and services (ex. Health)"
+
+                # },
+            },
+            'required': ['country_code']
+
+        }
+    },
+    {
+        'name': 'plot_CPI',
+        'description': 'Plots consumer price index of country in the given time interval.',
+        'parameters': {
+            'type': 'object',
+            'properties': {
+                'country_code': {
+                    'type': 'string',
+                    'description': 'Country code of the country, (for example TR for Turkey, and NL for the Netherlands)'
+
+                },
+                'start_year': {
+                    'type': 'string',
+                    'description': "The starting year value of desired the desired time interval"
+
+                },
+                'start_month': {
+                    'type': 'string',
+                    'description': "The starting month value of desired the desired time interval"
+
+                },
+                'end_year': {
+                    'type': 'string',
+                    'description': "The end year value of desired the desired time interval"
+
+                },
+                'end_month': {
+                    'type': 'string',
+                    'description': "The end month value of desired the desired time interval"
 
                 },
             },
@@ -291,7 +458,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Selam! Nasıl yardımcı olabilirim?"):
+if prompt := st.chat_input("Hi! How can I help you?"):
     start_time = datetime.now()
 
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -337,16 +504,30 @@ if prompt := st.chat_input("Selam! Nasıl yardımcı olabilirim?"):
             elif function_name in ['calculate_SMA', 'calculate_EMA']:
                 args_dict = {'ticker': function_args.get('ticker'), 'window': function_args.get('window')}
             elif function_name in ['get_CPI']:
-
                 year_value = function_args.get('year')
                 if year_value is None:
-                    year_value = 2024
+                    year_value = 2023
                 month_value = function_args.get('month')
                 if month_value is None:
-                    month_value = 1
-
+                    month_value = 12
                 args_dict = {'country_code': function_args.get('country_code'), 'year': year_value,
                              'month': month_value}
+            elif function_name in ['plot_CPI']:
+                start_year_value = function_args.get('start_year')
+                if start_year_value is None:
+                    start_year_value = 2000
+                start_month_value = function_args.get('start_month')
+                if start_month_value is None:
+                    start_month_value = 1
+                end_year_value = function_args.get('end_year')
+                if end_year_value is None:
+                    end_year_value = 2023
+                end_month_value = function_args.get('end_month')
+                if end_month_value is None:
+                    end_month_value = 12
+                args_dict = {'country_code': function_args.get('country_code'),
+                             'start_year': start_year_value, 'start_month': start_month_value,
+                             'end_year': end_year_value, 'end_month': end_month_value}
             elif function_name in ['report']:
                 args_dict = {}
 
@@ -355,6 +536,8 @@ if prompt := st.chat_input("Selam! Nasıl yardımcı olabilirim?"):
 
             if function_name == 'plot_stock_price':
                 st.image('stock.png')
+            elif function_name == 'plot_CPI':
+                st.image('CPI.png')
             else:
                 st.session_state['messages'].append(response_message)
                 st.session_state['messages'].append(
